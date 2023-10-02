ce0a8308ca4384360bdf0cc8d04bc2419a48ddb9
Nezu <126691412+devnezu@users.noreply.github.com>
2023-10-02 11:07:05 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ const scriptContent = `
     'Five star weapon': 'Armas [5⭐ Stars]',
     'Four star weapon': 'Armas [4⭐ Stars]',
     'Easy search': 'Procura Rápida',
-    'Enter more conditions to query data': 'Selecione mais opções para consultar dados'
+    'Enter more conditions to query data': 'Selecione mais algumas opções para exibir as contas disponíveis',
   };
 
   const translateContent = () => {
@@ -158,10 +158,22 @@ const scriptContent = `
       }
   }
 
+    function translateSpecialElements() {
+      // Seleciona o elemento com a classe 'desc' e o atributo 'data-v-08c57ff9'
+      const descElement = document.querySelector('.desc[data-v-08c57ff9]');
+      
+      // Verifica se o elemento foi encontrado
+      if (descElement) {
+          // Altera o texto dentro do elemento
+          descElement.textContent = 'Selecione mais algumas opções para exibir as contas disponíveis';
+      }
+  }
+
     document.addEventListener('DOMContentLoaded', () => {
         translateContent();
         setTimeout(adjustLanguageSelector, 3000);
         setTimeout(selectServerAmerica, 3000);  
+        setTimeout(translateSpecialElements , 3000);
         const copyButton = document.querySelector('.copy-btn');
         if (copyButton) {
             copyButton.addEventListener('click', () => {
@@ -228,20 +240,6 @@ const scriptContent = `
           }
       });
   }
-
-  const replaceImage = () => {
-    setTimeout(() => {
-      const oldImageSrc = "/genshin/static/img/nodata.9844e18.png";
-      const newImageSrc = "https://media.discordapp.net/attachments/1071788462858047511/1149143239484510249/unnamed.webp?ex=651c0cca&is=651abb4a&hm=64e1a03020538cee4f6b20553e426e2461b6a4e90abd18f53566edac4639acc0&=";
-      const imageElement = document.querySelector(\`img[src='${oldImageSrc}']\`);
-      if (imageElement) {
-        imageElement.setAttribute('src', newImageSrc);
-      }
-    }, 10000);  // 10 segundos de atraso
-  };
-  
-  document.addEventListener('DOMContentLoaded', replaceImage);
-  
   
 `;
  
