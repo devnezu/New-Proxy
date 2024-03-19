4ec5c4ebe5437876d727be2d90560697f22fcf00
Nezu <126691412+devnezu@users.noreply.github.com>
2024-03-18 23:34:29 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -52,143 +52,204 @@ function modifyContent() {
   module.exports = { modifyContent, copyToClipboard, translateCharacters };
   
 
-const scriptContent = `
-const style = document.createElement('style');
-style.innerHTML = \`${styleContent}\`; // Substitua styleContent pelo conteúdo CSS desejado
-document.head.appendChild(style);
-
-const link = document.createElement('link');
-link.href = 'https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap';
-link.rel = 'stylesheet';
-link.defer = true;
-document.head.appendChild(link);
-
-const translationMap = {
-  'Servidor': 'Servidor da Conta',
-  'Número_Limitado_de_Personagens': 'Número de Personagens Limitados',
-  'Número_Limitado_de_Armas': 'Número de Personagens do Mochileiro',
-  'Número_de_armas': 'Número de Armas 5Estrelas',
-  'Gênero': 'Gênero do Viajante',
-  'Masculino': 'Masculino',
-  'Feminino': 'Feminino',
-  'Linguagem': 'Idioma',
-  'Constellation level': 'Level de Constelação',
-  'Destino Entrelaçado': 'Destinos Entrelaçados',
-  'Gema Essencial': 'Quantidade de Gemas',
-  'Apenas 5 estrelas selecionados': 'Apenas os limitados selecionados',
-  'Apenas 4 estrelas selecionados': 'Apenas os mochileiros selecionados',
-  'Personagens_5_Estrelas': 'Personagens [5⭐ Stars]',
-  'Personagens_4_Estrelas': 'Personagens [4⭐ Stars]',
-  'Armas_5_Estrelas': 'Armas [5⭐ Stars]',
-  'Armas_4_Estrelas': 'Armas [4⭐ Stars]',
-  'Pesquisa': 'Combinações Rápidas',
-  'Sem_Dados': 'Selecione mais algumas opções para exibir as contas disponíveis',
-  'Procurar': 'Buscar',
-  'Resetar': 'Limpar',
-  'the life': 'C1',
-  'Second life': 'C2',
-  'Three lives': 'C3',
-  'Four lives': 'C4',
-  'Five lives': 'C5',
-  'Six lives': 'C6',
-};
-
-const translateContent = () => {
-  document.body.style.zoom = "75%";
-
-  setTimeout(() => {
-    const elements = document.querySelectorAll('.name, .el-select-dropdown__item span, .el-checkbox__label, .el-tabs__item, .desc, .btnback span');
-    elements.forEach(element => {
-      const textContent = element.textContent.trim();
-      if (translationMap[textContent]) {
-        element.textContent = translationMap[textContent];
-      }
-    });
-  }, 2000);
-};
-
-const selectServerAmerica = () => {
-  setTimeout(() => {
-    const serverDropdownItems = document.querySelectorAll('.el-select-dropdown__item span');
-    const americaItem = Array.from(serverDropdownItems).find(item => item.textContent.trim().toLowerCase() === 'america');
-    if (americaItem) {
-      americaItem.click();
-    }
-  }, 3000);
-};
-
-function translateCharacters(str) {
+  const scriptContent = `
+  const style = document.createElement('style');
+  style.innerHTML = \`${styleContent}\`;
+  document.head.appendChild(style);
+  
+  const link = document.createElement('link');
+  link.href = 'https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap';
+  link.rel = 'stylesheet';
+  link.defer = true;
+  document.head.appendChild(link);
+  
+  const translationMap = {
+    'Servidor': 'Servidor da Conta',
+    'Número_Limitado_de_Personagens': 'Número de Personagens Limitados',
+    'Número_Limitado_de_Armas': 'Número de Personagens do Mochileiro',
+    'Número_de_armas': 'Número de Armas 5Estrelas',
+    'Gênero': 'Gênero do Viajante',
+    'Masculino': 'Masculino',
+    'Feminino': 'Feminino',
+    'Linguagem': 'Idioma',
+    'Constellation level': 'Level de Constelação',
+    'Destino Entrelaçado': 'Destinos Entrelaçados',
+    'Gema Essencial': 'Quantidade de Gemas',
+    'Apenas 5 estrelas selecionados': 'Apenas os limitados selecionados',
+    'Apenas 4 estrelas selecionados': 'Apenas os mochileiros selecionados',
+    'Personagens_5_Estrelas': 'Personagens [5⭐ Stars]',
+    'Personagens_4_Estrelas': 'Personagens [4⭐ Stars]',
+    'Armas_5_Estrelas': 'Armas [5⭐ Stars]',
+    'Armas_4_Estrelas': 'Armas [4⭐ Stars]',
+    'Pesquisa': 'Combinações Rápidas',
+    'Sem_Dados': 'Selecione mais algumas opções para exibir as contas disponíveis',
+    'Procurar': 'Buscar',
+    'Resetar': 'Limpar',
+    'the life': 'C1',
+    'Second life': 'C2',
+    'Three lives': 'C3',
+    'Four lives': 'C4',
+    'Five lives': 'C5',
+    'Six lives': 'C6',
+  };
+  
   const characterTranslationMap = {
     '男': 'Masculino',
     '女': 'Feminino',
     '级': '',
   };
-
-  for (const [key, value] of Object.entries(characterTranslationMap)) {
-    str = str.replace(new RegExp(key, 'g'), value);
-  }
-
-  return str;
-}
-
-function modifyText(text) {
-  let modifiedText = text.replace('女', 'Lumine').replace('男', 'Aether').replace('级', '');
-  return modifiedText;
-}
-
-function copyToClipboard(content) {
-  navigator.clipboard.writeText(content)
-    .then(() => console.log('Conteúdo copiado para a área de transferência.'))
-    .catch(err => console.error('Erro ao copiar para a área de transferência:', err));
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.style.filter = 'blur(5px)';
-
-  selectServerAmerica();
-  translateContent();
-
-  setTimeout(() => {
-    const shoppingDiv = document.querySelector('div.shoppaing.animate__animated.animate__bounce');
-    if (shoppingDiv) {
-      shoppingDiv.style.visibility = 'hidden';
-    }
-
-    const languageDivs = Array.from(document.querySelectorAll('.inpItem .name'));
-    const languageDiv = languageDivs.find(div => div.textContent.trim() === 'Idioma');
-    if (languageDiv) {
-      languageDiv.closest('.inpItem').style.visibility = 'hidden';
-    }
-
-    const topLanguageDiv = document.querySelector('.top_language');
-    if (topLanguageDiv) {
-      topLanguageDiv.style.visibility = 'hidden';
-    }
-
+  
+  const translateContent = () => {
+    document.body.style.zoom = "100%";
+  
     setTimeout(() => {
-      document.body.style.filter = 'none';
-    }, 1000);
-
-  }, 10000);
-
-  const copyButton = document.querySelector('.copy-btn');
-  if (copyButton) {
-    copyButton.addEventListener('click', () => {
-      const selectedText = document.getSelection().toString();
-      const modifiedContent = translateCharacters(selectedText);
-      copyToClipboard(modifiedContent);
-    });
-  }
-});
-
-document.addEventListener('copy', (event) => {
-  event.preventDefault();
-  const selectedText = document.getSelection().toString();
-  const translatedText = translateCharacters(selectedText);
-  event.clipboardData.setData('text/plain', translatedText);
-});
-
-    
-`;
-
-module.exports = scriptContent;
+      const elements = document.querySelectorAll('.name, .el-select-dropdown__item span, .el-checkbox__label, .el-tabs__item, .desc, .btnback span');
+      elements.forEach(element => {
+        const textContent = element.textContent.trim();
+        if (translationMap[textContent]) {
+          element.textContent = translationMap[textContent];
+        }
+      });
+    }, 2000);
+  };
+  
+  const selectServerAmerica = () => {
+    setTimeout(() => {
+      const serverDropdownItems = document.querySelectorAll('.el-select-dropdown__item span');
+      const americaItem = Array.from(serverDropdownItems).find(item => item.textContent.trim().toLowerCase() === 'america');
+      if (americaItem) {
+        americaItem.click();
+      }
+    }, 3000);
+  };
+  
+  const translateCharacters = str => {
+    for (const [key, value] of Object.entries(characterTranslationMap)) {
+      str = str.replace(new RegExp(key, 'g'), value);
+    }
+    return str;
+  };
+  
+  const modifyText = text => {
+    return text.replace('女', 'Lumine').replace('男', 'Aether').replace('级', '');
+  };
+  
+  const copyToClipboard = content => {
+    navigator.clipboard.writeText(content)
+      .then(() => console.log('Conteúdo copiado para a área de transferência.'))
+      .catch(err => console.error('Erro ao copiar para a área de transferência:', err));
+  };
+  
+  const createLoadingScreen = () => {
+    const loadingScreen = document.createElement('div');
+    loadingScreen.id = 'loadingScreen';
+    loadingScreen.style.cssText = \`
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 100vh;
+      background-color: white;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      z-index: 1000;
+    \`;
+    loadingScreen.innerHTML = \`
+      <div style="display: flex; align-items: center;">
+        <div class="spinner" style="margin-right: 20px;"></div>
+        <span style="font-size: 2rem; color: black;">Carregando... Por favor, aguarde.</span>
+      </div>
+    \`;
+    const spinnerStyle = document.createElement('style');
+    spinnerStyle.innerHTML = \`
+      .spinner {
+        border: 5px solid #f3f3f3;
+        border-top: 5px solid #3498db;
+        border-radius: 50%;
+        width: 50px;
+        height: 50px;
+        animation: spin 2s linear infinite;
+      }
+      @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+      }
+    \`;
+    document.head.appendChild(spinnerStyle);
+    document.body.appendChild(loadingScreen);
+  };
+  
+  const removeLoadingScreen = () => {
+    const loadingScreen = document.getElementById('loadingScreen');
+    if (loadingScreen) {
+      loadingScreen.remove();
+    }
+  };
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    createLoadingScreen();
+    selectServerAmerica();
+    translateContent();
+  
+    setTimeout(() => {
+      const shoppingDiv = document.querySelector('div.shoppaing.animate__animated.animate__bounce');
+      if (shoppingDiv) {
+        shoppingDiv.style.visibility = 'hidden';
+      }
+  
+      const languageDivs = Array.from(document.querySelectorAll('.inpItem .name'));
+      const languageDiv = languageDivs.find(div => div.textContent.trim() === 'Idioma');
+      if (languageDiv) {
+        languageDiv.closest('.inpItem').style.visibility = 'hidden';
+      }
+  
+      const topLanguageDiv = document.querySelector('.top_language');
+      if (topLanguageDiv) {
+        topLanguageDiv.style.visibility = 'hidden';
+      }
+  
+      const elementsToUnblur = document.querySelectorAll('body > *');
+      elementsToUnblur.forEach(element => {
+        element.dataset.blur = 5;
+      });
+  
+      const intervalId = setInterval(() => {
+        let allCleared = true;
+        elementsToUnblur.forEach(element => {
+          let blurValue = parseFloat(element.dataset.blur);
+          blurValue = Math.max(0, blurValue - 0.5);
+          element.style.filter = 'blur(' + blurValue + 'px)';
+          element.dataset.blur = blurValue;
+  
+          if (blurValue > 0) {
+            allCleared = false;
+          }
+        });
+  
+        if (allCleared) {
+          clearInterval(intervalId);
+          removeLoadingScreen();
+        }
+      }, 100);
+    }, 3000);
+  
+    const copyButton = document.querySelector('.copy-btn');
+    if (copyButton) {
+      copyButton.addEventListener('click', () => {
+        const selectedText = document.getSelection().toString();
+        const modifiedContent = translateCharacters(selectedText);
+        copyToClipboard(modifiedContent);
+      });
+    }
+  });
+  
+  document.addEventListener('copy', event => {
+    event.preventDefault();
+    const selectedText = document.getSelection().toString();
+    const translatedText = translateCharacters(selectedText);
+    event.clipboardData.setData('text/plain', translatedText);
+  });
+  `;
+  
+  module.exports = scriptContent;
